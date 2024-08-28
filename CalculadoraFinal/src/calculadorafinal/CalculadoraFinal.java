0f33639603205d1d2e106ccdbfe6b8b50bdea581
Carlos-Guirola <guirolacarlos745@gmail.com>
2024-08-28 08:45:52 -0600

    Calculadora Version 1
    Calculadora completa  progamada en java

---
+++ CalculadoraFinal/src/calculadorafinal/CalculadoraFinal.java
@@ -0,0 +1,13 @@
+package calculadorafinal;
+
+public class CalculadoraFinal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      Formulario f = new Formulario();
+      f.setVisible(true);
+    }
+    
+}
